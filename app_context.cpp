ec0c0dd2d05ff9c5733676de3a0fce2ee60da300
Natalia Salazar <nataliasalazardo@gmail.com>
2026-06-18 13:57:33 -0600

    Update app_context.cpp

--- app_context.cpp
+++ app_context.cpp
@@ -1,6 +1,8 @@
 #include "app_context.hpp"
 namespace App
 {
+    // Estructura que almacena los handles de todas las tareas
     Handles g_handles;
+    // Estructura que almacena los handles de las colas del sistema
     Queues g_queues;
 }
